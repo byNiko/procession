3d89d641affe8bed78135215ea3b3df8946e3e2f
nick <nick@byniko.com>
2023-07-28 05:37:43 -0700

    update flexible events filter

--- template-parts/flexible-layouts/flexible-event_listing.php
+++ template-parts/flexible-layouts/flexible-event_listing.php
@@ -1,11 +1,10 @@
 <?php
-$filter = get_sub_field('event_type')->slug;
-// print_r($filter['slug']);
-$futureEvents = get_scheduled_events('future', [$filter]);
+$filter = get_sub_field('event_type');
+$futureEvents = get_scheduled_events('future', [$filter->slug]);
 if ($futureEvents) :
 ?>
 	<div class="upcoming-workshops mt-5">
-		<h2 class="mb-5">Upcoming Workshops</h2>
+		<h2 class="mb-5">Upcoming <?php echo $filter->name; ?> Events</h2>
 		<?php
 		foreach ($futureEvents as $post) : setup_postdata($post);
 			get_template_part('template-parts/content', get_post_type());
