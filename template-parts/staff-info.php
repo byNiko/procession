48b2c3d6282c0e54c09f34a1419c4b4b261e2d32
nick <nick@byniko.com>
2023-07-28 05:37:10 -0700

    added extra line to staff

--- template-parts/staff-info.php
+++ template-parts/staff-info.php
@@ -4,19 +4,21 @@
 	<!-- </header> -->
 	<?php
 	if (have_rows('staff', 'option')) :  ?>
-		<dl class="staff-members row">
-			<?php while (have_rows('staff', 'option')) : the_row(); ?>
-				<div class="staff-member column">
-					<dt class="staff--name">
-						<?php _e(the_sub_field('first_name')); ?>
-						<?php _e(the_sub_field('last_name')); ?>
-					</dt>
-					<dd class="staff--title">
-						<?php _e(the_sub_field('role')); ?>
-					</dd>
-				</div>
-			<?php endwhile; ?>
-		</dl>
+	<dl class="staff-members row">
+		<?php while (have_rows('staff', 'option')) : the_row(); ?>
+		<div class="staff-member column">
+			<dt class="staff--name">
+				<?php _e(the_sub_field('first_name')); ?>
+				<?php _e(the_sub_field('last_name')); ?>
+			</dt>
+			<dd class="staff--title">
+				<div><?php _e(the_sub_field('role')); ?></div>
+				<div><?php _e(the_sub_field('extra_info')); ?></div>
+
+			</dd>
+		</div>
+		<?php endwhile; ?>
+	</dl>
 </section>
 <?php
 	endif;
