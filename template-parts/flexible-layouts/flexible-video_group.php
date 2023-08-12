fc34b0edc58ea16e247cb179148378f8f1a082f6
nick <nick@byniko.com>
2023-08-12 07:03:01 -0700

    update flexible video

--- template-parts/flexible-layouts/flexible-video_group.php
+++ template-parts/flexible-layouts/flexible-video_group.php
@@ -1,14 +1,14 @@
 <?php
 $group = get_sub_field('video_group');
-if ($group && $group['vimeo_id']) :
+if ($group && $group['vimeo_id'] && $group['padding-top']) :
 ?>
 <div class="container">
 	<div class="row">
 		<div class="column">
-			<div style="padding:56.25% 0 0 0;position:relative;">
+			<div class="flexible-video--container" style="padding:<?php echo $group['padding-top']; ?> 0 0 0;">
 				<iframe src="https://player.vimeo.com/video/<?php echo $group['vimeo_id']; ?>?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;autoplay=1&amp;muted=1&amp;playsinline=1"
 						frameborder="0" allow="autoplay; fullscreen; picture-in-picture"
-						style="position:absolute;top:0;left:0;width:100%;height:100%;" title="video1747494092"></iframe>
+						title="video1747494092"></iframe>
 			</div>
 			<script src="https://player.vimeo.com/api/player.js"></script>
 		</div>
